6374c2de73c1a699b7bf0da617afb1d52dde9158
laaggan <lagergrenlinus@gmail.com>
2021-01-27 09:30:04 +0100

    You can now add new users from frontend

--- testingReact/ClientApp/src/components/AddNewUser.js
+++ testingReact/ClientApp/src/components/AddNewUser.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 export class AddNewUser extends Component {
     static displayName = AddNewUser.name;
@@ -12,26 +12,46 @@ export class AddNewUser extends Component {
       <div>
         <h1>Add new user to website</h1>
 
-        <NewUserForm />
+            <NewUserForm
+                AddUserToDb={AddUserToDb}
+            />
       </div>
     );
   }
 }
 
+async function AddUserToDb(user) {
+    const response = await fetch(
+        'api/Home/addNewUser?userName=' + user.UserName + "&age=" + user.Age, {
+            method: 'post'
+    }
+    );
+}
+
 const NewUserForm = (props) => {
+    const [userName, setUserName] = useState('Nisse');
+    const [age, setAge] = useState(50);
+
+    const cleanForm = () => {
+        setUserName('');
+        setAge('');
+    }
+
     return (
         <>
-            <form class="form-group">
+            <form className="form-group">
             <label>
                     Name:
-            <input class="form-control" type="text" name="name" />
+            <input className="form-control" type="text" name="name" value={userName} onChange={e => setUserName(e.target.value)} />
                 </label>
             <label>
                     Age:
-            <input class="form-control" type="text" name="name" />
+            <input className="form-control" type="text" name="name" value={age} onChange={e => setAge(e.target.value)}/>
             </label>
             </form>
-            <button style={{ display: 'block' }} className="btn btn-primary">Add user</button>
+            <button style={{ display: 'block' }} className="btn btn-primary" onClick={() => props.AddUserToDb({ UserName: userName, Age: age }).then(cleanForm())}>Add user</button>
+            <p> {userName} </p>
+            <p> {age} </p>
         </>
         )
 }
