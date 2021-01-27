de56066392ddce0315439f2dee5801fee1a5eb04
laaggan <lagergrenlinus@gmail.com>
2021-01-27 13:07:34 +0100

    Added view model for tweets

---
+++ testingReact/TweetViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testingReact
+{
+    public class TweetViewModel
+    {
+        public string Tweet { get; set; }
+        public string UserName { get; set; }
+    }
+}
